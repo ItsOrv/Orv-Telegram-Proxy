b69ef9277c6f54194509e26128bc58cbb6323f3b
ItsOrv <poriya.saw@gmail.com>
2024-08-08 21:40:28 +0330

    set monitored channel id and fix support link

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ client = TelegramClient('session_name', api_id, api_hash)
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
 
-@client.on(events.NewMessage(chats=[]))
+@client.on(events.NewMessage(chats=[-1001234567890]))
 async def my_event_handler(event):
     message = event.message.message
     proxy_links = re.findall(r'https?://t\.me/proxy\?\S+', message)
@@ -27,7 +27,7 @@ async def my_event_handler(event):
                 location = response.json().get('country', 'idk')
                 if len(server) > 16:
                     server = server[:16] + '.etc'
-                text = f"**Orv\n\n• Country: {location} \n• IP: {server} \n• Port: {port} \n\n**[proxy](https://t.me/Orv_Proxy)~[config](https://t.me/Orv_Vpn)~[bot](https://t.me/OrBSup_bot)~[support](https://t.me/OrvSup_bot)"
+                text = f"**Orv\n\n• Country: {location} \n• IP: {server} \n• Port: {port} \n\n**[proxy](https://t.me/Orv_Proxy)~[config](https://t.me/Orv_Vpn)~[bot](https://t.me/OrBSup_bot)~[support](https://t.me/Orv_Sup)"
                 buttons = [[KeyboardButtonUrl('Connect', link)]]
                 await bot.send_message('orv_proxy', text, buttons=buttons, link_preview=False)
             except (AttributeError, requests.RequestException) as e:
