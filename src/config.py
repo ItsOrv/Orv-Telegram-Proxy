0a93af13d75ffb226f229aa608d91b6b5b6d27ef
ItsOrv <poriya.saw@gmail.com>
2024-08-18 16:42:30 +0330

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+# config.py
+
 import os
 from dotenv import load_dotenv
 
@@ -6,11 +8,13 @@ load_dotenv()
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
-channel_id = os.getenv('CHANNEL_ID') #orv_proxy
+#str
+channel_id = os.getenv('CHANNEL_ID')
 
-proxy_channel_url = os.getenv('PROXY_CHANNEL_URL') #https://t.me...
-config_channel_url = os.getenv('CONFIG_CHANNEL_URL') #https://t.me...
-bot_url = os.getenv('BOT_URL') #https://t.me...
-support_url = os.getenv('SUPPORT_URL') #https://t.me...
+proxy_channel_url = os.getenv('PROXY_CHANNEL_URL')
+config_channel_url = os.getenv('CONFIG_CHANNEL_URL')
+bot_url = os.getenv('BOT_URL')
+support_url = os.getenv('SUPPORT_URL')
 
-channels = os.getenv('CHANNELS').split(',') #12342342 234124 12341234 1524213 12341234
+# Load channels as a list of integers
+channels = [int(chat_id.strip()) for chat_id in os.getenv('CHANNELS').split(',')]
