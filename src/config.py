aafec877717b42712863a3c09d97451f6d7bc1db
ItsOrv <poriya.saw@gmail.com>
2024-08-18 15:52:49 +0330

    add channels to config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,3 @@
-# config.py
-
 import os
 from dotenv import load_dotenv
 
@@ -8,9 +6,11 @@ load_dotenv()
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
-channel_id = int(os.getenv('CHANNEL_ID'))  # Ensure channel_id is an integer
+channel_id = os.getenv('CHANNEL_ID') #orv_proxy
 
-proxy_channel_url = os.getenv('PROXY_CHANNEL_URL')
-config_channel_url = os.getenv('CONFIG_CHANNEL_URL')
-bot_url = os.getenv('BOT_URL')
-support_url = os.getenv('SUPPORT_URL')
+proxy_channel_url = os.getenv('PROXY_CHANNEL_URL') #https://t.me...
+config_channel_url = os.getenv('CONFIG_CHANNEL_URL') #https://t.me...
+bot_url = os.getenv('BOT_URL') #https://t.me...
+support_url = os.getenv('SUPPORT_URL') #https://t.me...
+
+channels = os.getenv('CHANNELS').split(',') #12342342 234124 12341234 1524213 12341234
